131b130a05f296677ab136821c3a9c4889d3276b
ryan <370187428@qq.com>
2018-04-14 17:17:09 +0800

    下班了

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ryan
+ * Date: 2018/4/14
+ * Time: 17:06
+ */
+
+namespace App\Http\Controllers;
+
+class TestController extends Controller{
+
+
+    public function haha()
+    {
+        echo __METHOD__;
+    }
+}
